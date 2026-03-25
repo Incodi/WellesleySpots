61fb5c188319fa0da1880ced9df49ab16c4e12f1
Gloria P <gp103@wellesley.edu>
2026-03-25 16:16:52 -0400

    updating review page and server.js

---
+++ server.js
@@ -0,0 +1,44 @@
+const path = require('path');
+require("dotenv").config({ path: path.join(process.env.HOME, '.cs304env')});
+const express = require('express');
+const morgan = require('morgan');
+const serveStatic = require('serve-static');
+const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+
+// our modules loaded from cwd
+
+const { Connection } = require('./connection');
+const cs304 = require('./cs304');
+
+// Create and configure the app
+
+const app = express();
+
+// Morgan reports the final status code of a request's response
+app.use(morgan('tiny'));
+
+app.use(cs304.logStartRequest);
+
+// This handles POST data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(cs304.logRequestData);  // tell the user about any request data
+
+app.use(serveStatic('public'));
+app.set('view engine', 'ejs');
+
+const mongoUri = cs304.getMongoUri();
+const DB = 'WellesleySpots';
+const USERS = 'users';
+const LOCATIONS = 'locations';
+const REVIEWS = 'reviews';
+const COMMENTS = 'comments';
+
+app.get('/', (req, res) => {
+    return res.render('./reviews/reviewpage.ejs');
+});
+
+
+
