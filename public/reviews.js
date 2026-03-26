e6f93ecc931beb3b3d319061f896c1682f45a9d7
Gloria P <gp103@wellesley.edu>
2026-03-25 21:04:41 -0400

    adding base map in reviews using leaflet

---
+++ public/reviews.js
@@ -0,0 +1,17 @@
+const map = L.map('map').setView([42.293243, -71.305604], 16);
+
+const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
